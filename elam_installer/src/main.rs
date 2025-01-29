9a36a7fa7d050fd08df70c36563561a3c2d913e1
flux <49762827+0xflux@users.noreply.github.com>
2025-01-29 07:52:11 +0000

    PPL AntiMalware service runner

---
+++ elam_installer/src/main.rs
@@ -0,0 +1,111 @@
+use std::ptr::null_mut;
+
+use windows::{core::{PCSTR, PCWSTR}, Win32::{Storage::FileSystem::{CreateFileW, FILE_ATTRIBUTE_NORMAL, FILE_READ_DATA, FILE_SHARE_READ, OPEN_EXISTING}, System::{Antimalware::InstallELAMCertificateInfo, Services::{ChangeServiceConfig2W, CreateServiceW, OpenSCManagerA, OpenSCManagerW, SC_MANAGER_ALL_ACCESS, SERVICE_CONFIG_LAUNCH_PROTECTED, SERVICE_DEMAND_START, SERVICE_ERROR_NORMAL, SERVICE_LAUNCH_PROTECTED_ANTIMALWARE_LIGHT, SERVICE_LAUNCH_PROTECTED_INFO, SERVICE_WIN32_OWN_PROCESS}}}};
+
+fn main() {
+    //
+    // Step 1:
+    // Install the ELAM certificate via the driver (.sys) file.
+    //
+    println!("[i] Starting Elam installer..");
+
+    let mut path: Vec<u16> = vec![];
+    r"C:\Users\flux\AppData\Roaming\Sanctum\sanctum.sys".encode_utf16().for_each(|c| path.push(c));
+    path.push(0);
+
+    // todo un hanrdcode this
+    let result = unsafe {
+        CreateFileW(
+            PCWSTR(path.as_ptr()),
+            FILE_READ_DATA.0, 
+            FILE_SHARE_READ, 
+            None, 
+            OPEN_EXISTING, 
+            FILE_ATTRIBUTE_NORMAL, 
+            None,
+        )
+    };
+
+    let handle = match result {
+        Ok(h) => h,
+        Err(e) => panic!("[!] An error occurred whilst trying to open a handle to the driver. {e}"),
+    };
+
+    if let Err(e) = unsafe { InstallELAMCertificateInfo(handle) } {
+        panic!("[!] Failed to install ELAM certificate.");
+    }
+
+    println!("[+] ELAM certificate installed successfully!");
+
+
+    //
+    // Step 2:
+    // Create a service with correct privileges
+    //
+
+    println!("[i] Attempting to create the service.");
+    let result = unsafe {
+        OpenSCManagerW(
+            PCWSTR::null(), 
+            PCWSTR::null(), 
+            SC_MANAGER_ALL_ACCESS,
+        )
+    };
+
+    let h_sc_mgr = match result {
+        Ok(h) => h,
+        Err(e) => panic!("[!] Unable to open SC Manager. {e}"),
+    };
+
+    // create an own process service
+    
+    let result = unsafe {
+        CreateServiceW(
+            h_sc_mgr, 
+            PCWSTR(svc_name().as_ptr()),
+            PCWSTR(svc_name().as_ptr()),
+            SC_MANAGER_ALL_ACCESS, 
+            SERVICE_WIN32_OWN_PROCESS, // Service that runs in its own process
+            SERVICE_DEMAND_START, 
+            SERVICE_ERROR_NORMAL, 
+            PCWSTR(svc_bin_path().as_ptr()), 
+            PCWSTR::null(), None, PCWSTR::null(), PCWSTR::null(), PCWSTR::null()
+        )
+    };
+
+    let h_svc = match result {
+        Ok(h) => h,
+        Err(e) => panic!("[!] Failed to create service. {e}"),
+    };
+
+    let mut info= SERVICE_LAUNCH_PROTECTED_INFO::default();
+    info.dwLaunchProtected = SERVICE_LAUNCH_PROTECTED_ANTIMALWARE_LIGHT;
+    
+    if let Err(e) = unsafe { ChangeServiceConfig2W(
+        h_svc, 
+        SERVICE_CONFIG_LAUNCH_PROTECTED, 
+        Some(&mut info as *mut _ as *mut _)
+    )} {
+        panic!("[!] Error calling ChangeServiceConfig2W. {e}");
+    }
+
+    println!("[+] Successfully initialised the PPL AntiMalware service. It now needs staring with `net.exe start sanctum_ppl_runner`");
+
+    
+}
+
+fn svc_name() -> Vec<u16> {
+    let mut svc_name: Vec<u16> = vec![];
+    "sanctum_ppl_runner".encode_utf16().for_each(|c| svc_name.push(c));
+    svc_name.push(0);
+
+    svc_name
+}
+
+fn svc_bin_path() -> Vec<u16> {
+    let mut svc_path: Vec<u16> = vec![];
+    // todo not hardcode
+    r"C:\Users\flux\AppData\Roaming\Sanctum\sanctum_ppl_runner.exe".encode_utf16().for_each(|c| svc_path.push(c));
+    svc_path.push(0);
+    svc_path
+}
---
+++ sanctum_ppl_runner/src/main.rs
@@ -0,0 +1,79 @@
+//! A service runner for the Protected Process Lite Antimalware which allows us to interact with ETW:TI
+
+use std::{sync::atomic::{AtomicBool, Ordering}, thread::sleep, time::Duration};
+
+use windows::{core::PCWSTR, Win32::{Foundation::ERROR_SUCCESS, System::Services::{RegisterServiceCtrlHandlerW, SetServiceStatus, SERVICE_RUNNING, SERVICE_START_PENDING, SERVICE_STATUS, SERVICE_STATUS_CURRENT_STATE, SERVICE_STATUS_HANDLE, SERVICE_STOPPED, SERVICE_WIN32_OWN_PROCESS}}};
+
+static SERVICE_STOP: AtomicBool = AtomicBool::new(false);
+
+/// The service entrypoint for the binary which will be run via powershell / persistence
+#[unsafe(no_mangle)]
+pub unsafe extern "system" fn ServiceMain(_: u32, _: *mut *mut u16) {
+    // register the service with SCM (service control manager)
+    let h_status = match unsafe {RegisterServiceCtrlHandlerW(
+        PCWSTR(svc_name().as_ptr()), 
+        Some(service_handler)
+    )} {
+        Ok(h) => h,
+        Err(e) => panic!("[!] Could not register service. {e}"),
+    };
+
+    // notify SCM that service is starting
+    unsafe { update_service_status(h_status, SERVICE_START_PENDING.0) };
+
+    // start the service main loop
+    run_service(h_status);
+
+}
+
+
+/// Main service execution loop
+fn run_service(h_status: SERVICE_STATUS_HANDLE) {
+    unsafe {
+        update_service_status(h_status, SERVICE_RUNNING.0);
+
+        // Main loop
+        while !SERVICE_STOP.load(Ordering::SeqCst) {
+            sleep(Duration::from_secs(1)); // Simulated workload
+        }
+
+        // Cleanup and notify SCM of service stop
+        update_service_status(h_status, SERVICE_STOPPED.0);
+    }
+}
+
+
+fn svc_name() -> Vec<u16> {
+    let mut svc_name: Vec<u16> = vec![];
+    "sanctum_ppl_runner".encode_utf16().for_each(|c| svc_name.push(c));
+    svc_name.push(0);
+    
+    svc_name
+}
+
+/// Handles service control events (e.g., stop)
+unsafe extern "system" fn service_handler(control: u32) {
+    match control {
+        SERVICE_CONTROL_STOP => {
+            SERVICE_STOP.store(true, Ordering::SeqCst);
+        }
+        _ => {}
+    }
+}
+
+/// Update the service status in the SCM
+unsafe fn update_service_status(h_status: SERVICE_STATUS_HANDLE, state: u32) {
+    let mut service_status = SERVICE_STATUS {
+        dwServiceType: SERVICE_WIN32_OWN_PROCESS,
+        dwCurrentState: SERVICE_STATUS_CURRENT_STATE(state),
+        dwControlsAccepted: if state == SERVICE_RUNNING.0 { 1 } else { 0 },
+        dwWin32ExitCode: ERROR_SUCCESS.0,
+        dwServiceSpecificExitCode: 0,
+        dwCheckPoint: 0,
+        dwWaitHint: 0,
+    };
+
+    unsafe {SetServiceStatus(h_status, &mut service_status)};
+}
+
+fn main() {}
